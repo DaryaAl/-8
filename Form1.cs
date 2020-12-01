8eac780f67f785e427a655495e25a24d5309d041
исп-31 <-31@LAB17-06>
2020-12-01 10:05:26 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Lib_6;
+
+namespace Практическая_работа__8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Кнопка О программе
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Создать интерфейсы – человек и печать (для формирования информации об объекте)." +
+                "Создать класс – студент.Класс должен включать конструкторы функцию для формирования строки информации о студенте." +
+                "Сравнение производить по фамилии.");
+        }
+
+        //Кнопка Выход
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //Создание объектов
+        Student student1 = new Student();
+        Student student2 = new Student();
+
+        //Кнопка Заполнить данные о студенте 1
+        private void button1_Click(object sender, EventArgs e)
+        {
+            student1.FirstName = textBox1.Text;
+            student1.LastName = textBox3.Text;
+            student1.Patronymic = textBox2.Text;
+            student1.Department = textBox4.Text;
+            student1.Age = Convert.ToInt32(textBox6.Text);
+            student1.Course = Convert.ToInt32(textBox5.Text);
+            listBox1.Items.Add(student1.Print().ToString());
+        }
+
+        //Кнопка Заполнить данные о студенте 2
+        private void button2_Click(object sender, EventArgs e)
+        {
+            student2.FirstName = textBox7.Text;
+            student2.LastName = textBox10.Text;
+            student2.Patronymic = textBox8.Text;
+            student2.Department = textBox12.Text;
+            student2.Age = Convert.ToInt32(textBox9.Text);
+            student2.Course = Convert.ToInt32(textBox11.Text);
+            listBox2.Items.Add(student2.Print().ToString());
+        }
+
+        //Кнопка Клонировать
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Student student2 = (Student)student1.Clone();
+            listBox2.Items.Add(student2.Print().ToString());
+        }
+
+        //Кнопка Сравнить
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (student1.CompareTo(student2) == 1)
+            {
+                MessageBox.Show("Фамилии студентов одинаковые");
+            }
+            else
+            {
+                MessageBox.Show("Фамилии студентов не одинаковые");
+            }
+        }
+
+        //Кнопка Очистить
+        private void очиститьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+            textBox7.Clear();
+            textBox8.Clear();
+            textBox9.Clear();
+            textBox10.Clear();
+            textBox11.Clear();
+            textBox12.Clear();
+        }
+
+        //Очистить информацию о первом студенте
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+
+        //Очистить информацию о втором студенте
+        private void button6_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+        }
+    }
+}
---
+++ Lib_6/Class1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lib_6
+{
+    //Интерфейс Человек
+    interface iPerson
+    {
+        string FirstName { get; set; }//свойство Имя
+        string LastName { get; set; }//свойство Фамилия
+        string Patronymic { get; set; }//свойство Отчество
+        int Age { get; set; }//свойство Возраст
+
+    }
+
+
+    //Интерфейс Печать
+    interface iPrint
+    {
+        string Print();//метод Печать
+    }
+
+
+
+    //Класс Student, в котором определены интерфейсы iPerson, iPrint, IComparable, ICloneable
+    public class Student : iPerson, iPrint, IComparable, ICloneable
+    {
+        public int course;//поле Курс
+        public int Course//свойство 
+        {
+            get
+            {
+                return course;
+            }
+            set
+            {
+                if (value > 0 && value <= 4)
+                {
+                    course = value;
+                }
+                else
+                {
+                    MessageBox.Show("Курс должен быть 1, 2, 3 или 4");
+                }
+            }
+        }
+        public int age;//поле Возраст
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value > 15)
+                {
+                    age = value;
+                }
+                else
+                {
+                    MessageBox.Show("Недопустимый возраст, укажите правильно");
+                }
+            }
+        }
+
+        public string FirstName { get; set; }//Поле Имя
+        public string LastName { get; set; }//Поле Фамилия
+        public string Patronymic { get; set; }//Поле Отчество
+        public string Department { get; set; }//Поле Отделение
+
+
+        //Метод Печать
+        public string Print()
+        {
+            //возвращает строку
+            return "Имя: " +  FirstName + " , " + "Фамилия: " + LastName + " , " + " Отчество: " + Patronymic + " , " + " Курс " + Course + " , " + " Отделение: " + Department + " , " + " Возраст: " + Age;
+        }
+
+        //метод сравнения
+        public int CompareTo(object obj)
+        {
+            Student student = (Student)obj;
+            if (this.LastName == student.LastName) { return 1; }
+            return 0;
+        }
+
+        //метод клонирования
+        public object Clone()
+        {
+            Student st = new Student();
+            st.FirstName = this.FirstName;
+            st.LastName = this.LastName;
+            st.Patronymic = this.Patronymic;
+            st.Department = this.Department;
+            st.Age = this.Age;
+            st.Course = this.Course;
+            return st;
+        }
+    }
+}
